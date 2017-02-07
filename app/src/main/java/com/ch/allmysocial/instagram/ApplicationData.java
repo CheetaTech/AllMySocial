8157df20827f74eab63f007a3a2df4a6372fe4d8
cheetatech <cheetatech@gmail.com>
2017-02-08 00:29:57 +0300

    instagram API integration

---
+++ app/src/main/java/com/ch/allmysocial/instagram/ApplicationData.java
@@ -0,0 +1,13 @@
+package com.ch.allmysocial.instagram;
+
+/**
+ * Created by Ali.Guvenbas on 5.2.2017.
+ */
+
+public class ApplicationData {
+
+
+    public static final String CLIENT_ID = "1f054c24caa84918a7e5876319bb8d9d";
+    public static final String CLIENT_SECRET = "bdbc0d3c9b7b44ae9e94769fe78a6eb7";
+    public static final String CALLBACK_URL = "http://www.cheetatech.com/";
+}
